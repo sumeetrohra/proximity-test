9adff7cc256541e24d1da4d3b0651525512e89de
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-15 15:49:01 +0000

    Deploy sumeetrohra/proximity-test to github.com/sumeetrohra/proximity-test.git:gh-pages

---
+++ precache-manifest.7eb7bf453d43f35c1a75e5c9d3074b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a6f6ba1e5c3e314312eb3bebc89a99",
+    "url": "/proximity-test/index.html"
+  },
+  {
+    "revision": "8fa93bc785197b531512",
+    "url": "/proximity-test/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8fa93bc785197b531512",
+    "url": "/proximity-test/static/js/2.e0ca9925.chunk.js"
+  },
+  {
+    "revision": "7cd2e048a3be58e6922d93c69f118710",
+    "url": "/proximity-test/static/js/2.e0ca9925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a771a280112ee25865",
+    "url": "/proximity-test/static/js/main.245e21ec.chunk.js"
+  },
+  {
+    "revision": "f0defe379a7aa057de86",
+    "url": "/proximity-test/static/js/runtime-main.cdc57b41.js"
+  }
+]);
